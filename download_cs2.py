7cc1bc515da1c8e0a2cbb26280722b9b402c2236
vodracseck <79907142+wwidlishy@users.noreply.github.com>
2024-12-01 17:43:38 +0100

    Project Remake
    Pros:
    - Fixed downloader (no longer requires manifest files)
    - Tidied up shit
    - Made a mapcompile script generator
    Cons:
    - download of one depot requires login
    - i wanna die
    - if doesnt work sarim no proud :(

---
+++ download_cs2.py
@@ -0,0 +1,30 @@
+import glob
+import subprocess
+import shutil
+import os
+
+print("Downloading of last nessesary depot requires to be logged into steam")
+uname = input("steam user name: ")
+password = input("steam password: ")
+
+path = "Counter-Strike Global Offensive"
+
+cmds = [
+        f'DepotDownloader.exe DATA'.replace("DATA", i)
+        for i in [
+            "-app 730 -depot 2347770 -manifest 1473979371368005478",
+            "-app 730 -depot 2347771 -manifest 734640093393352243",
+            f"-app 730 -depot 2347779 -manifest 3859439092678459074 -username {uname} -password {password}"
+        ]
+]
+
+print("[...] Downloading (this may take a while)")
+
+for i in cmds:
+    os.system(i)
+
+print("Downloaded!\n[...] Patching")
+
+api64_path = path + "\\game\\bin\\win64"
+shutil.copy("steam_api64.dll", api64_path)
+print("Patched!")
---
+++ make_mapcompile_script.py
@@ -0,0 +1,18 @@
+base = \
+"""@echo off
+set CS2install="Counter-Strike Global Offensive"
+set ProjectName=$PNAME
+set MapName=$MNAME
+"%CS2install%\\game\\bin\\win64\\resourcecompiler.exe" -threads 5 -fshallow -maxtextureres 256 -dxlevel 110 -quiet -unbufferedio -i "%CS2install%/content/csgo_addons/%ProjectName%/maps/%MapName%.vmap"  -noassert  -world -bakelighting -lightmapMaxResolution 1024 -lightmapDoWeld -lightmapVRadQuality 1 -lightmapLocalCompile -phys -vis -nav -retail -breakpad -nop4 -outroot "%AppData%\\..\\Local\\Temp\\valve\\hammermapbuild\\game" -lightmapcpu
+xcopy "%AppData%\\..\\Local\\Temp\\valve\\hammermapbuild\\game\\csgo_addons\\%ProjectName%\\maps\\%MapName%.vpk" "%CS2install%\\game\\csgo_addons\\%ProjectName%\\maps\\" /y
+pause"""
+
+print("Do not move the bat file created!")
+
+pname = input("Project name: ")
+mname = input("Map file name: ")
+
+with open(f"Compile {mname} in {pname}.bat", "w") as w:
+    w.write(base.replace("$MNAME", mname).replace("$PNAME", pname))
+
+print(f"Script made: Compile {mname} in {pname}.bat")
