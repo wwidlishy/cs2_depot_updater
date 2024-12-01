bb6d7c4e47a5dd6d8796f17e529a503644fc7dd1
vodracseck <79907142+wwidlishy@users.noreply.github.com>
2024-12-01 17:50:54 +0100

    Update make_mapcompile_script.py

--- make_mapcompile_script.py
+++ make_mapcompile_script.py
@@ -1,6 +1,6 @@
 base = \
 """@echo off
-set CS2install="Counter-Strike Global Offensive"
+set CS2install=Counter-Strike Global Offensive
 set ProjectName=$PNAME
 set MapName=$MNAME
 "%CS2install%\\game\\bin\\win64\\resourcecompiler.exe" -threads 5 -fshallow -maxtextureres 256 -dxlevel 110 -quiet -unbufferedio -i "%CS2install%/content/csgo_addons/%ProjectName%/maps/%MapName%.vmap"  -noassert  -world -bakelighting -lightmapMaxResolution 1024 -lightmapDoWeld -lightmapVRadQuality 1 -lightmapLocalCompile -phys -vis -nav -retail -breakpad -nop4 -outroot "%AppData%\\..\\Local\\Temp\\valve\\hammermapbuild\\game" -lightmapcpu
